4c23d12058cb6a399feb3a92f7bb21218c37afd9
mseo39 <a01063294905@gmail.com>
2020-08-14 18:37:24 +0900

    8.14

--- project1/myapp/views.py
+++ project1/myapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from myapp.models  import Deginer
+from myapp.models  import Deginer #모델의 존재 알려주기
 # Create your views here.
 
 def home(request):
+    #queryset을 templates로 보내기
     deginers = Deginer.objects.all()
-    return render(request,'home.html', {'deginer':deginers})
+    return render(request,'home.html', {'deginers':deginers})
     #전해줄 값을 딕셔너리로 작성함
 def introduce(request):
     return render(request,'introduce.html')
